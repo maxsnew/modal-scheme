f7fb1703b90f4f1068485cdbe2102cd3bd1d26bc
Max New <maxsnew@gmail.com>
2018-12-19 18:05:58 -0500

    8a

--- advent/8/solution.rkt
+++ advent/8/solution.rkt
@@ -7,7 +7,10 @@
 
 (provide main-a main-b)
 
-;; A Tree is a (Node ,(U (CoList Node)) ,(U (CoList Nat))
+;; A StackOn A B is one of
+;;   F (cl-cons A (U Stackon A B))
+;;   CoList B
+;; A Tree is a (Node (StackOn Tree Nat))
 ;; 
 
 ;; Tokenize
@@ -15,8 +18,32 @@
   (! <<n
      list<-colist 'o cl-map parse-num 'o sep-by #\space 'o read-all-chars))
 
+;; Nat ... -> F Nat
+(def-thunk (! parse)
+  (letrec
+   ;; (U(Nat -> 'stk -> Nat ... -> F Nat) -> Nat -> 'stk -> Nat -> Nat -> ... -> F Nat
+   ([tree
+     (~ (copat [(k tot (= 'stk) children# metadata#)
+                [k = (~ (! metadata k metadata#))]
+                (! forest k children# tot 'stk)]))]
+    [metadata
+     (~ (copat
+         [(k     (= 0) tot (= 'stk)) (! k tot 'stk)]
+         [(k metadata# tot (= 'stk) n)
+          [metadata# <- (! - metadata# 1)] [tot <- (! + tot n)]
+          (! metadata k metadata# tot 'stk)]))]
+    [forest
+     (~ (copat
+         [(k (= 0) tot (= 'stk)) (! k tot 'stk)]
+         [(k children# tot (= 'stk))
+          [children# <- (! - children# 1)]
+          [k = (~ (! forest k children#))]
+          (! tree k tot 'stk)]))])
+    (! tree abort 0 'stk)))
+
 (def-thunk (! main-a)
-  (! nums))
+  [ns <- (! nums)]
+  (! apply parse ns))
 
 (def-thunk (! main-b)
   (ret 'not-done-yet))
