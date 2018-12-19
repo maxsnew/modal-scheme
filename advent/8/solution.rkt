adc21bea6e499bd60a512fc2422ecb00af66bd7d
Max New <maxsnew@gmail.com>
2018-12-19 18:32:00 -0500

    checking in 8b, not done yet

--- advent/8/solution.rkt
+++ advent/8/solution.rkt
@@ -4,6 +4,7 @@
 (require "../IO.rkt")
 (require "../CoList.rkt")
 (require "../Parse.rkt")
+(require "../Stream.rkt")
 
 (provide main-a main-b)
 
@@ -45,5 +46,44 @@
   [ns <- (! nums)]
   (! apply parse ns))
 
+(def-thunk (! parse-b)
+  (letrec
+   ;; (U(Nat -> 'stk -> Nat ... -> F Nat) -> Nat -> 'stk -> Nat -> Nat -> ... -> F Nat
+   ([tree ;; I only need to know my destination and the total so far
+     (~ (copat [(k tot (= 'stk) children# metadata#)
+                [k <-
+                   (cond
+                     [(! equal? 0 children#)
+                      (ret (~ (! add-metadata k metadata#)))]
+                     [else (ret (~ (! deref-metadata k metadata#)))])]
+                (! forest k children# '() tot 'stk)]))]
+    [deref-metadata
+     (~ (copat
+         [(k     (= 0) tots tot (= 'stk)) (! k tot 'stk)]
+         [(k metadata# tots tot (= 'stk) ix)
+          [metadata# <- (! - metadata# 1)]
+          [tot <- (! <<v + tot 'o stream-ref tots ix)]
+          (! deref-metadata k metadata# tots tot 'stk)]))]
+    [add-metadata
+     (~ (copat
+         [(k     (= 0) _ tot (= 'stk)) (! k tot 'stk)]
+         [(k metadata# _ tot (= 'stk) n)
+          [metadata# <- (! - metadata# 1)] [tot <- (! + tot n)]
+          (! add-metadata k metadata# #f tot 'stk)]))]
+    [forest
+     (~ (copat
+         [(k (= 0) tots tot (= 'stk))
+          [tots <- (! reverse tots)]
+          (! k (~ (! stream<-list tots)) tot 'stk)]
+         [(k children# tots tot (= 'stk))
+          [children# <- (! - children# 1)]
+          [k = (~
+                (copat
+                 [(tot (= 'stk))
+                  (! forest k children# (cons tot tots) tot 'stk)]))]
+          (! tree k tot 'stk)]))])
+    (! tree abort 0 'stk)))
+
 (def-thunk (! main-b)
-  (ret 'not-done-yet))
+  [ns <- (! nums)]
+  (! apply parse-b ns))
