ba8b39472c7691db9abc78fcd90baf935b5cff26
Max New <maxsnew@gmail.com>
2019-08-14 12:09:48 -0400

    full basic interpreter, y combinator example

--- fiddle/src/Fiddle.hs
+++ fiddle/src/Fiddle.hs
@@ -1,28 +1,38 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
 module Fiddle where
 
 import Control.Monad.Except
 import Control.Monad.Writer
 
+import qualified Data.Map.Strict as Map
+import Data.Map.Strict (Map)
+
+import qualified Data.Text as T
+import Data.Text (Text)
+
 -- | AST
 data Value
   = VBool Bool
-  | VThnk Computation
-  | VVal String
-
+  | VThunk Computation
+  | VVar Var
+  deriving (Show, Read, Eq, Ord)
 data Computation
-  = CForce Value
-  | CReturn Value
-  | CBind Computation String Computation
+  = CReturn Value
+  | CBind Computation Var Computation
+  | CForce Value
   | CIf Value Computation Computation
-  | CLambda String Computation
+  | CLambda Var Computation
   | CApp Computation Value
+  | CCaseLam Computation Var Computation
   | CPrint Value Computation
-
-newtype RTErr = RTErr { displayErr :: String }
+  deriving (Show, Read, Eq, Ord)
+type Var = Text
+newtype RTErr = RTErr { displayErr :: Text }
   deriving (Show, Read, Eq, Ord)
 
-mkErr :: String -> RTErr
+mkErr :: Text -> RTErr
 mkErr = RTErr
 
 newtype Trace = Trace { readOut :: [Bool] }
@@ -48,20 +58,39 @@ runFiddler = swap . runWriter . runExceptT . unFiddler
 instance Show SemComp where
   show (SemComp ret tA) = "SemComp (" ++ show ret ++ ") *function*"
 
-type Values = [(String, SemVal)]
+type Values = Map Var SemVal
 
 interpV :: Value -> Values -> Either RTErr SemVal
 interpV v g = case v of
   VBool b -> return $ SemBool b
-  _ -> Left (RTErr "not yet implemented")
+  VThunk c -> return $ SemThunk (interpC c g)
+  VVar x -> maybe (Left $ RTErr ("Variable " <> x <> " not in scope.")) Right $ Map.lookup x g
+interpRet :: Value -> Values -> SemComp
+interpRet v g = case interpV v g of
+  Left e  -> errC e
+  Right v -> retC v
 
 interpC :: Computation -> Values -> SemComp
 interpC c g = case c of
-  CReturn v -> case interpV v g of
-                 Left  e -> errC e
-                 Right v -> retC v
-  CBind c x k -> bindC (interpC c g) (\v -> interpC k ((x,v) : g))
-  _ -> errC $ RTErr "not yet implemented"
+  CReturn v -> interpRet v g
+  CBind c x k -> bindC (interpC c g) (\v -> interpC k (Map.insert x v g))
+  CForce v -> bindC (interpRet v g) $ \case
+    SemThunk c -> c
+    v       -> errC $ mkErr "Tried to force a non-thunk"
+  CIf v tk fk -> bindC (interpRet v g) $ \case
+    SemBool b -> (`interpC` g) $ if b then tk else fk
+    v         -> errC $ mkErr "Tried to do an if on a non-bool"
+  CLambda x m -> lamC $ \v -> interpC m (Map.insert x v g)
+  CApp m v    -> bindC (interpRet v g) $ takeArg (interpC m g)
+  CCaseLam r x m -> SemComp
+    { returner = returner $ interpC r g
+    , takeArg  = \v -> interpC m (Map.insert x v g)
+    }    
+  CPrint v m  -> bindC (interpRet v g) $ \case
+    SemBool b -> printAll (single b) $ interpC m g
+    v         -> errC $ mkErr "Tried to print a non-bool"
+  where
+    single b = Trace [b]
 
 retC :: SemVal -> SemComp
 retC v = SemComp
@@ -79,6 +108,12 @@ bindC :: SemComp -> (SemVal -> SemComp) -> SemComp
 bindC c k = case (runFiddler $ returner c) of
               (t, mayV) -> printAll t $ either errC k mayV
 
+lamC :: (SemVal -> SemComp) -> SemComp
+lamC f = SemComp
+  { returner = throwError . mkErr $ "Tried to grab an arg on the stack, but there was none"
+  , takeArg = f
+  }
+
 printAll :: Trace -> SemComp -> SemComp
 printAll t c = SemComp
   { returner = tell t >> returner c
@@ -89,3 +124,27 @@ printAll t c = SemComp
 -- b `printThen` c = SemComp
 --   { returner = tell ()
 --   }
+
+-------------------------------
+-- | Examples
+-------------------------------
+tru = VBool True
+fls = VBool False
+ret = CReturn
+prnt = CPrint
+ex1 = ret tru
+ex2 = prnt tru $ prnt fls $ ret fls
+lam = CLambda
+
+w :: Value -> Computation
+w t = CLambda x (CApp (CForce t) (VThunk $ CApp (CForce (VVar x)) (VVar x)))
+  where x = "x"
+y = CLambda f $ CApp (w (VVar f)) (VThunk (w (VVar f)))
+  where f = "f"
+
+recC :: Var -> Computation -> Computation
+recC x m = CApp y (VThunk $ CLambda x m)
+
+constantly = CLambda "x" $ recC "constantly" $ CCaseLam (ret (VVar "x")) "_" (CForce (VVar "constantly"))
+
+pretty c = runFiddler $ returner c
