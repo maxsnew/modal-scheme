83f0d0f0154437d1cab12873d851a23416296519
Max New <maxsnew@gmail.com>
2019-12-06 15:52:47 -0500

    [advent 2019] 03b

--- stdlib/advent-2019/03/solution.rkt
+++ stdlib/advent-2019/03/solution.rkt
@@ -45,59 +45,53 @@
 ;; (list 'Vert x lo-y hi-y) where x and lo-y <= hi-y are numbers
 ;; (list 'Hor  lo-x hi-x y) where lo-x <= hi-x and y are numbers
 
-(def-thunk (! apply-vector start vector)
+(def-thunk (! apply-vector start vector distance)
   [x <- (! first start)]
   [y <- (! second start)]
   [dir <- (! first vector)]
   [n <- (! second vector)]
+  [new-distance <- (! + n distance)]
   ((copat [((= #\U))
            [hi-y <- (! + y n)]
-           [line <- (! List 'Vert x y hi-y)]
+           [line <- (! List 'Vert x y hi-y y distance)]
            [new-pt <- (! List x hi-y)]
-           (! List line new-pt)]
+           (! List line new-pt new-distance)]
           [((= #\D))
            [lo-y <- (! - y n)]
-           [line <- (! List 'Vert x lo-y y)]
+           [line <- (! List 'Vert x lo-y y y distance)]
            [new-pt <- (! List x lo-y)]
-           (! List line new-pt)]
+           (! List line new-pt new-distance)]
           [((= #\L))
            [lo-x <- (! - x n)]
-           [line <- (! List 'Hor  lo-x x y)]
+           [line <- (! List 'Hor lo-x x y x distance)]
            [new-pt <- (! List lo-x y)]
-           (! List line new-pt)]
+           (! List line new-pt new-distance)]
           [((= #\R))
            [hi-x <- (! + x n)]
-           [line <- (! List 'Hor x hi-x y)]
+           [line <- (! List 'Hor x hi-x y x distance)]
            [new-pt <- (! List hi-x y)]
-           (! List line new-pt)])
+           (! List line new-pt new-distance)])
    dir))
 
 ;; U (CoList Direction) -> CoList Lines
 (def-thunk (! lines<-directions dirs)
   [next-line
    = (~ (copat
-         [(dir k start)
-          [line*new-pt <- (! apply-vector start dir)]
+         [(dir k start distance-traveled)
+          [line*new-pt*new-distance <- (! apply-vector start dir distance-traveled)]
           (! apply (~ (copat
-                       [(line new-pt)
-                        (! cl-cons line (~ (! k new-pt)))]))
-             line*new-pt)]))]
+                       [(line new-pt new-distance)
+                        (! cl-cons line (~ (! k new-pt new-distance)))]))
+             line*new-pt*new-distance)]))]
   (! cl-foldr dirs next-line
-     (~ (λ (xy) (! cl-nil)))
-     '(0 0)))
+     (~ (λ (xy distance) (! cl-nil)))
+     '(0 0) 0))
 
 (def-thunk (! inside? x lo hi)
   (! and (~ (! <= lo x))
          (~ (! <= x hi))))
 
-(def-thunk (! intersect-orthogonal x lo-y hi-y y lo-x hi-x)
-  (cond [(! and (~ (! inside? x lo-x hi-x))
-                (~ (! inside? y lo-y hi-y)))
-         (! <<v List 'o List x y '$)]
-        [else
-         (ret '())]))
-
-(def-thunk (! intersection l1 l2)
+(def-thunk (! distance*intersection-pairs l1 l2)
   [or1 <- (! first l1)]
   [or2 <- (! first l2)]
   ;; (! displayall 'intersecting l1 l2)
@@ -112,15 +106,32 @@
            [lo-x <- (! second l2)]
            [hi-x <- (! third l2)]
            [y <- (! fourth l2)]
-           (! intersect-orthogonal x lo-y hi-y y lo-x hi-x)]
+           (cond [(! and (~ (! inside? x lo-x hi-x))
+                         (~ (! inside? y lo-y hi-y)))
+                  [start-x <- (! fifth l2)]
+                  [start-y <- (! fifth l1)]
+                  [hor-dist <- (! sixth l2)]
+                  [vert-dist <- (! sixth l1)]
+                  [tot-dist
+                   <- (! idiom (~ (ret +))
+                         (~ (! + hor-dist vert-dist))
+                         (~ (! <<v abs 'o - start-x x))
+                         (~ (! <<v abs 'o - start-y y)))]
+                  (! idiom (~ (ret List))
+                     (~ (! idiom (~ (ret List)) (~ (ret tot-dist)) (~ (! List x y)))))]
+                 [else
+                  (ret '())])]
           [((= 'Hor) (= 'Vert))
-           (! intersection l2 l1)])
+           (! distance*intersection-pairs l2 l1)])
    or1 or2))
+
+(def-thunk (! intersection l1 l2)
+  (! <<v map second 'o distance*intersection-pairs l1 l2 '$))
 
 (def-thunk (! manhattan-to-origin x y)
   (! idiom (~ (ret +)) (~ (! abs x)) (~ (! abs y))))
 
-(def-thunk (! main-a)
+(def-thunk (! all-line-pairs)
   [wires <- (! parse-input)]
   (! displayall 'parsed)
   [w1 <- (! first wires)]
@@ -128,11 +139,16 @@
   [lines1 = (~ (! lines<-directions (~ (! colist<-list w1))))]
   [lines2 = (~ (! lines<-directions (~ (! colist<-list w2))))]
   [line-pairs = (~ (! cartesian-product lines1 lines2))]
+  (ret line-pairs)
+  )
+
+(def-thunk (! main-a)
+  [line-pairs <- (! all-line-pairs)]
+  ;; [_ <- (! list<-colist line-pairs)]
+  ;; (! displayall 'not-the-line-pairs-fualt)
   [not-both-zero =
                  (~ (copat [(x y)
                             (! <<v not 'o and (~ (! zero? x)) (~ (! zero? y)) '$)]))]
-  ;; #;(! list<-colist line-pairs)
-  ;; (! intersect-orthogonal 3 8 5 6 3 7)
   (! <<n
    minimum-by (~ (! apply manhattan-to-origin)) '(+inf.0 +inf.0) 'o
    cl-filter (~ (! apply not-both-zero)) 'o
@@ -141,8 +157,19 @@
         (do [pts <- (! apply intersection line*line)]
             (! cl-append (~ (! colist<-list pts)) tl))))
    cl-nil
-   '$
-     ))
+   '$))
 
 (def-thunk (! main-b)
-  (ret 'not-done-yet))
+  [line-pairs <- (! all-line-pairs)]
+  ;; [_ <- (! list<-colist line-pairs)]
+  ;; (! displayall 'not-the-line-pairs-fualt)
+  [non-zero-dist = (~ (copat [(x y) (! <<v not 'o zero? x '$)]))]
+  (! <<n
+   minimum-by first '(+inf.0 something-is-wrong) 'o
+   cl-filter (~ (! apply non-zero-dist)) 'o
+   cl-foldr line-pairs
+   (~ (λ (line*line tl)
+        (do [pts <- (! apply distance*intersection-pairs line*line)]
+            (! cl-append (~ (! colist<-list pts)) tl))))
+   cl-nil
+   '$))
