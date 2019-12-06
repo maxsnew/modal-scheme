b46ea98f9cafec3cf3db5abbabe9bdcc28622c86
Max New <maxsnew@gmail.com>
2019-12-06 15:55:50 -0500

    [advent 2019] init 04

---
+++ stdlib/advent-2019/04/solution.rkt
@@ -0,0 +1,14 @@
+#lang sbpv
+
+(require "../../../stdlib.rkt")
+(require "../../IO.rkt")
+(require "../../CoList.rkt")
+(require "../../Parse.rkt")
+
+(provide main-a main-b)
+
+(def-thunk (! main-a)
+  (ret 'not-done-yet))
+
+(def-thunk (! main-b)
+  (ret 'not-done-yet))
